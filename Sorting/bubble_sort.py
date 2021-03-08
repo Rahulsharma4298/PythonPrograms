86bdb56c9ce8a38e0efc2ad4f4091a36297b175f
unknown <rahulsharma4298@gmail.com>
2021-03-08 20:12:08 +0530

    Sorting: Optimized Bubble Sort

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -1,10 +1,10 @@
 def bubble_sort(arr):
     for i in range(len(arr)-1):
-        for j in range(len(arr)-1):
+        for j in range(len(arr)-i-1):
             if arr[j]>arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-                print(arr)
+    return arr
             
    
-    
-bubble_sort([4,2,6,8,2,9,104,89,101])
+arr = [4,2,6,8,2,9,104,89,101] 
+print(bubble_sort(arr))
