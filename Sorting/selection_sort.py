37f0e98574434a88ff16776ecd88ca194cb2f060
unknown <rahulsharma4298@gmail.com>
2021-03-08 20:12:47 +0530

    Sorting: Added Selection Sort

---
+++ Sorting/selection_sort.py
@@ -0,0 +1,11 @@
+def selection_sort(arr):
+    for i in range(len(arr)):
+        min_idx = i
+        for j in range(i+1, len(arr)):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        arr[i], arr[min_idx] = arr[min_idx], arr[i]
+    return arr
+
+arr = [5,8,1,4,7,2]
+print(selection_sort(arr))
